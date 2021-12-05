dedc08c5fce26e16b7509628ba114c055dc0aabf
acadjuric <acadjuric98@gmail.com>
2021-12-06 00:15:21 +0100

    Initial page for file upload

---
+++ FrontEnd/ppee/src/Components/FileUpload.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class FileUpload extends Component {
+    render() {
+        return (
+            <div>
+                
+                <div className="file-container">
+                    <h3 className="file-heading">File Upload </h3> 
+                    <input type="file"></input>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FileUpload;
---
+++ FrontEnd/ppee/src/Components/Navigation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+
+    render() {
+
+        return (
+            <div className="navigation">
+                <a href="#">File upload</a>
+                <a href="#">Training</a>
+                <a href="#">Report</a>
+            </div>
+        );
+    }
+}
+
+export default Navigation;
