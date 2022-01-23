0ef9e98809634dd4c29ddb9f20c8b093849fe650
acadjuric <acadjuric98@gmail.com>
2022-01-23 05:07:26 +0100

    Optimization -design improvement

--- FrontEnd/ppee/src/Components/Optimization.js
+++ FrontEnd/ppee/src/Components/Optimization.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Modal from './Modal';
 import delteTrash from '../assets/delete1.png'
-import saveIcon from '../assets/save-icon.png'
+// import saveIcon from '../assets/save-icon.png'
 import { baseUrl } from '../constants';
 import axios from 'axios';
 
@@ -14,13 +14,16 @@ const generators = [
 
 ]
 
+const generatorsAndTheirNumberForOptimization = [];
+
 class Optimization extends Component {
     constructor() {
         super();
         this.state = {
             showModal: false,
             myGenerators: generators,
-            showSettings: false,
+            showSettings: true,
+            isMouseInside: false,
         }
     }
 
@@ -34,7 +37,7 @@ class Optimization extends Component {
         axios.get(baseUrl + "home/getpowerplant").then(response => {
 
             console.log("Server odgovorio-> ", response);
-            this.setState({myGenerators : response.data})
+            this.setState({ myGenerators: JSON.parse(response.data) })
 
         }).catch(error => {
             console.log(error);
@@ -49,29 +52,56 @@ class Optimization extends Component {
         this.setState({ showSettings: !this.state.showSettings });
     }
 
-    deleteGenerator = (index) => {
-        if (generators[index] === undefined)
-            return;
-
-        console.log(generators[index]);
+    deleteGenerator = (id) => {
+        console.log(id);
+        axios.delete(baseUrl + "home/deletepowerplant/" + id).then(response => {
+            console.log(response);
+            this.getAllGenerators();
+        }).catch(error => {
+            console.log(error);
+        })
     }
 
+    handleMouseEnter = () => {
+        this.setState({ isMouseInside: true })
+    }
+
+    handleMouseLeave = () => {
+        this.setState({ isMouseInside: false })
+    }
     SaveNumberOfGenerators = (index) => {
 
-        if (this.state.myGenerators[index] === undefined)
-            return;
+        // if (this.state.myGenerators[index] === undefined)
+        //     return;
 
-        console.log(this.state.myGenerators[index]);
+        // console.log(this.state.myGenerators[index]);
     }
 
-    handleInputChange = (index, event) => {
-        if (generators[index] === undefined)
-            return;
+    handleInputChange = (id, event) => {
+        console.log(id);
+        var modelExist = generatorsAndTheirNumberForOptimization.find(x => x.id === id);
+        console.log(modelExist)
+        if (modelExist === undefined) {
+            var model = {
+                id: id,
+                number: event.target.value,
+            }
 
-        console.log(generators[index]);
-        generators[index].numberForOptimization = event.target.value;
-        console.log(generators[index]);
-        this.setState({ myGenerators: generators })
+            generatorsAndTheirNumberForOptimization.push(model)
+        }
+        else {
+            var index = generatorsAndTheirNumberForOptimization.indexOf(modelExist);
+            modelExist.number = event.target.value
+            generatorsAndTheirNumberForOptimization[index] = modelExist
+        }
+
+        console.log(generatorsAndTheirNumberForOptimization);
+
+
+        // console.log(generators[index]);
+        // generators[index].numberForOptimization = event.target.value;
+        // console.log(generators[index]);
+        // this.setState({ myGenerators: generators })
     }
 
     render() {
@@ -93,36 +123,95 @@ class Optimization extends Component {
                     <div className='settings-container'>
 
                         <div className='basic-settings'>
-                            <button className='button-43' onClick={this.toggleModal}>Add generator</button>
-                            <h1> ovde ide izbor kirterijumske funkcije i podesavanje cene goriva kao i emisije CO2 </h1>
+                            {/* <button className='button-43' onClick={this.toggleModal}>Add generator</button> */}
+
+                            <div className='settings-column'>
+                                <div className='settings-box'>
+                                    <label>Coal fuel cost</label> <input type="number" />
+                                </div>
+
+                                <div className='settings-box'>
+                                    <label>Gas fuel cost</label> <input type="number" />
+                                </div>
+                            </div>
+                            <div className='settings-column'>
+                                <div className='settings-box'>
+                                    <label>Coal CO2 emission</label> <input type="number" />
+                                </div >
+
+                                <div className='settings-box'>
+                                    <label>Gas CO2 emission</label> <input type="number" />
+                                </div>
+                            </div>
+                            <div className='settings-column'>
+                                <div className='settings-box'>
+                                    <select className='settings-select'>
+                                        <option value="cost">Minimize production costs</option>
+                                        <option value="co2">Minimize CO2 emissions</option>
+                                        <option value="both">Fuel cost and CO2</option>
+                                    </select>
+                                </div>
+
+                                <div className='settings-box'>
+                                    <label>Fuel cost weight factor</label> <input type="number" />
+                                </div>
+                            </div>
+
                         </div>
 
                         <div className='card-container'>
                             {
                                 this.state.myGenerators && this.state.myGenerators.map((item, index) => {
                                     return (
-                                        <div className="card" key={index}>
-                                            <div className={"card-image card-image-" + item.type}></div>
+                                        <div className="card" key={index} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
+                                            <div className={"card-image card-image-" + item.Type}></div>
                                             <div className="card-text">
-                                                <h3>Max value</h3>
-                                                <p>{item.maximumOutputPower}</p>
 
-                                                <h3>Min value</h3>
-                                                <p>{item.minimumOutputPower}</p>
-                                                <h3>Optimization number</h3>
-                                                <input value={item.numberForOptimization} onChange={(event) => this.handleInputChange(index, event)} />
+                                                <div className="name-text">
 
-                                                <div className='card-BtnContainer'>
-
-                                                    <button className='BtnContainer-save' onClick={() => this.SaveNumberOfGenerators(index)}><img src={saveIcon} alt='save' /></button>
-
-                                                    <button className='BtnContainer-delete' onClick={() => this.deleteGenerator(index)}><img src={delteTrash} alt='delete' /></button>
-
+                                                    <h3>Name</h3>
+                                                    <p>{item.Name}</p>
                                                 </div>
+
+                                                <div className="max-text">
+                                                    <h3>Max value</h3>
+                                                    <p>{item.MaximumOutputPower} {item.Type === "solar" ? "[W]" : item.Type === "wind" ? "[KW]" : "[MW]"}</p>
+                                                </div>
+
+                                                {
+                                                    item.Type === "coal" || item.Type === "gas" ? (<div className="min-text"> <h3>Min value</h3> <p>{item.MinimumOutputPower} [MW] </p></div>) : null
+                                                }
+                                                {/* {item.Type === "coal" || item.Type === "gas" ? (<h3>Min value</h3>) : null}
+                                                {item.Type === "coal" || item.Type === "gas" ? (<p>{item.MinimumOutputPower} [MW] </p>) : null} */}
+
+
+                                                {
+                                                    item.Type === "solar" ? (<div className="min-text"> <h3>Efficiency</h3> <p>{item.Efficiency} [%] </p></div>) : null
+                                                }
+
+                                                {
+                                                    item.Type === "wind" ? (<div className="min-text"> <h3>Diameter</h3> <p>{item.BladesSweptAreaDiameter} [m] </p></div>) : null
+                                                }
+
+                                                <div className="number-text" >
+                                                    <h3>Optimization number</h3>
+                                                    <input onChange={(event) => this.handleInputChange(item.Id, event)} />
+                                                </div>
+
+
                                             </div>
-                                            <div className={"card-stats card-stats-" + item.type}>
+                                            <div className='card-BtnContainer'>
+
+                                                {/* <button className='BtnContainer-save' onClick={() => this.SaveNumberOfGenerators(item.Id)}><img src={saveIcon} alt='save' /></button> */}
+                                                {
+                                                    this.state.isMouseInside ? <button className='BtnContainer-delete' onClick={() => this.deleteGenerator(item.Id)}><img src={delteTrash} alt='delete' /></button> : null
+                                                }
+
+
+                                            </div>
+                                            <div className={"card-stats card-stats-" + item.Type}>
                                                 <div className="stat">
-                                                    <div className="type">{item.type}</div>
+                                                    <div className="type">{item.Type}</div>
                                                 </div>
                                             </div>
                                         </div>
